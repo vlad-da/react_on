2d38dcc90cb44ae9d27782b577e25d1f80ca5c6a
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2023-07-03 22:36:35 +0300

    Создаёт проброс данных и перебор элемента. Создаёт изменение цвета

--- project/src/components/employees-list/employees-list.js
+++ project/src/components/employees-list/employees-list.js
@@ -2,12 +2,18 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({info}) => {
+    //Возвращает новый массив  
+    const elements = info.map(el=> {
+        return (
+            <EmployeesListItem name={el.name} salary={el.salary} increase={el.increase}/>
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+
+           {elements}
         </ul>
     )
 }
