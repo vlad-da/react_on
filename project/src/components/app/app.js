c2cfb8cc031ca95539c720c718e6fa9422573c09
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2023-09-28 00:48:06 +0300

    Удаление пользователя

--- project/src/components/app/app.js
+++ project/src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,14 +8,32 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-  const data = [
-    {name: 'Saha V.', salary: '800', increase: false, id: 1},
-    {name: 'Timur S.', salary: '900', increase: false, id: 2},
-    {name: 'Vlad V.', salary: '1000', increase: true, id: 3},
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data : [
+        {name: 'Saha V.', salary: '800', increase: false, id: 1},
+        {name: 'Timur S.', salary: '900', increase: false, id: 2},
+        {name: 'Vlad V.', salary: '1000', increase: true, id: 3},
+      ]
+    }
+  }
 
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+      const index = data.findIndex((elem) => elem.id === id);
+      const before = data.slice(0, index);
+      const after = data.slice(index + 1);
+      const newArr = [...before, ...after];
 
+      return {
+        data: newArr
+      }
+    })
+  }
+
+render() {
   return (
     <div className="app">
         <AppInfo />
@@ -23,10 +43,13 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList info = {data}/>
+        <EmployeesList info = {this.state.data}
+                       onDelete={this.deleteItem}/>
         <EmployeesAddForm/>
     </div>
   );
+}
+  
 }
 
 export default App;
